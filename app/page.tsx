866089c34601efcec63cf0ff5cd6b09523318dd4
Ashik-Rahman100 <ashik5515@gmail.com>
2023-12-21 22:14:21 +0600

    added header & hero

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { FC, useState } from "react";
+import Header from "./components/Header";
+import Heading from "./utils/Heading";
+
+interface Props {}
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+
+  return (
+    <div>
+      <Heading
+        title="E-Learning"
+        description="E-Learning is a platform for students to learn and get help from teachers"
+        keywords="Programming,MERN,Redux,Machine Learning"
+      />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
+    </div>
+  );
+};
+
+export default Page;
